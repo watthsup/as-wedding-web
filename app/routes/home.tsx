945fd020ea13209f07f425a561a1ad91b0c63ffb
Annop Watthakovit <annop.wat@TDG-M0764.local>
2025-08-22 02:28:31 +0700

    Reorder photos

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -34,16 +34,18 @@ export default function Home() {
   // Image slideshow state
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const images = [
-    "/2.jpg",
+    "/11.jpg",
     "/3.jpg",
     "/4.jpg",
     "/5.jpg",
-    "/7.jpg",
     "/8.jpg",
+    "/2.jpg",
+    "/12.jpg",
     "/9.jpg",
     "/10.jpg",
-    "/11.jpg",
-    "/12.jpg",
+    "/13.jpg",
+    "/14.jpg",
+    "/15.jpg",
   ];
 
   // Preload images for smooth transitions
